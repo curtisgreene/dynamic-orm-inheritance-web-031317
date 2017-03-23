5740305e09b32ad6325a9ad7a3d3e59d42f3b18d
curtisgreene <curtis.j.w.greene@gmail.com>
2017-03-23 13:45:34 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,7 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def values_for_insert
+  def values_for_insert #wow
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
